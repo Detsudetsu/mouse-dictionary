9706c323cadcc9138cdbf2dd6a99e52cc142769f
detsu <36166146+Detsudetsu@users.noreply.github.com>
2022-05-20 01:47:07 +0900

    Delete workaround to query active tab

--- src/background/background.js
+++ src/background/background.js
@@ -16,14 +16,11 @@ if (BROWSER === "CHROME") {
   });
 } else {
   chrome.browserAction.onClicked.addListener(() => {
-    chrome.tabs.query({}, allTabs => {
-      const activeTabID = allTabs.filter(t => t.active)[0].id;
-      chrome.tabs.executeScript(activeTabID, {
-        file: "./main.js",
-      });
-      chrome.tabs.insertCSS(activeTabID, {
-        file: "./main.css",
-      });
+    chrome.tabs.executeScript({
+      file: "./main.js",
+    });
+    chrome.tabs.insertCSS({
+      file: "./main.css",
     });
   });
 }
